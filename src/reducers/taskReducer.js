b55db5727769d38b6bbe443e2bdebc2b6c426146
sakib-anik <sakib.within@gmail.com>
2025-07-22 07:00:15 +0600

    reducer_switch_case_done

--- src/reducers/taskReducer.js
+++ src/reducers/taskReducer.js
@@ -1,38 +1,43 @@
 export default function TaskReducer(tasks, action) {
-  if (action.type == "added") {
-    return [
-      ...tasks,
-      {
-        id: action.id,
-        text: action.text,
-        done: false,
-      },
-    ];
-  } else if (action.type == "changedText") {
-    return tasks.map((task) => {
-      if (task.id == action.id) {
-        return {
-          ...task,
+  switch (action.type) {
+    case "added": {
+      return [
+        ...tasks,
+        {
+          id: action.id,
           text: action.text,
-        };
-      } else {
-        return task;
-      }
-    });
-  } else if (action.type == "deleted") {
-    return tasks.filter((task) => task.id !== action.id);
-  } else if (action.type == "changedCheckbox") {
-    return tasks.map((task) => {
-      if (task.id == action.id) {
-        return {
-          ...task,
-          done: action.checked,
-        };
-      } else {
-        return task;
-      }
-    });
-  } else {
-    throw new Error(`${action.type} Action Doesn't Match`);
+          done: false,
+        },
+      ];
+    }
+    case "changedText": {
+      return tasks.map((task) => {
+        if (task.id == action.id) {
+          return {
+            ...task,
+            text: action.text,
+          };
+        } else {
+          return task;
+        }
+      });
+    }
+    case "deleted": {
+      return tasks.filter((task) => task.id !== action.id);
+    }
+    case "changedCheckbox": {
+      return tasks.map((task) => {
+        if (task.id == action.id) {
+          return {
+            ...task,
+            done: action.checked,
+          };
+        } else {
+          return task;
+        }
+      });
+    }
+    default:
+      throw new Error(`${action.type} Action Doesn't Match`);
   }
 }
